4d2f7f182c36b8708b9901178958b24b39b5fcfd
nammuung <audrbs2368@naver.com>
2024-03-15 12:21:02 +0900

    d

---
+++ src/views/AboutView.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="test">
+        <h1>This is a Test page.</h1>
+        <button id="te">test</button>
+    </div>
+</template>
+<script>
+axios
+    .get({
+        method: "get",
+        url: "http://localhost:81/notice/list",
+    })
+    .then(function (response) {})
+    .then(function () {});
+</script>
+
+<style>
+@media (min-width: 1024px) {
+    .about {
+        min-height: 100vh;
+        display: flex;
+        align-items: center;
+    }
+}
+</style>
